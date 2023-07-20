e65b8583009b762cf7b0fea4ed0bdb0d43df798e
XAfker <podlinskibartosz@gmail.com>
2023-07-20 13:46:21 +0200

    Certificates working

--- src/Components/Certificates/Certificates.jsx
+++ src/Components/Certificates/Certificates.jsx
@@ -1,68 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import './Certificates.css';
+import React, { useState, useEffect, useRef } from 'react';
+import './certificates.css';
 import Nav2 from '../Nav/Nav2';
+import Footer from '../Footer/Footer';
+import { Button } from 'react-bootstrap';
 
 function Certificates({ certificates }) {
-  const [selectedCertificate, setSelectedCertificate] = useState(null);
+  if (certificates.length === 0) {
+    return null;
+  }
+
   const [visibleCertificateIndex, setVisibleCertificateIndex] = useState(0);
+  const delay = 7000;
+  const intervalRef = useRef(null);
 
   useEffect(() => {
-    const delay = 300; 
-    setVisibleCertificateIndex(0); 
+    const totalCertificates = certificates.length;
 
     const interval = setInterval(() => {
-      setVisibleCertificateIndex((prevIndex) => {
-        if (prevIndex >= certificates.length - 1) {
-          clearInterval(interval);
-        }
-        return prevIndex + 1;
-      });
+      setVisibleCertificateIndex((prevIndex) => (prevIndex + 1) % totalCertificates);
     }, delay);
 
-    return () => clearInterval(interval);
-  }, [certificates]);
+    intervalRef.current = interval;
 
-  const handleCardClick = (certificate) => {
-    setSelectedCertificate((prevSelected) =>
-      prevSelected?._id === certificate._id ? null : certificate
-    );
+    return () => clearInterval(interval);
+  }, [certificates, delay]);
+
+  const handleCardClick = () => {
+    clearInterval(intervalRef.current);
+    setVisibleCertificateIndex((prevIndex) => (prevIndex + 1) % certificates.length);
+    intervalRef.current = setInterval(() => {
+      setVisibleCertificateIndex((prevIndex) => (prevIndex + 1) % certificates.length);
+    }, delay);
   };
 
   return (
-    <div className="certificates-container">
-      <div className="certificates-grid">
-        <Nav2 />
-        {certificates.map((certificate, index) => (
+    <div>
+      <Nav2 />
+      <div className="certificates-container">
+        <div className="certificates-grid">
           <CertificateCard
-            key={certificate._id}
-            certificate={certificate}
-            isSelected={selectedCertificate?._id === certificate._id}
-            isVisible={index === visibleCertificateIndex}
-            onClick={() => handleCardClick(certificate)}
+            key={certificates[visibleCertificateIndex]._id}
+            certificate={certificates[visibleCertificateIndex]}
+            onClick={handleCardClick}
           />
-        ))}
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
 
-function CertificateCard({ certificate, isSelected, isVisible, onClick }) {
+function CertificateCard({ certificate, isVisible, onClick }) {
+  const [flipped, setFlipped] = useState(false);
+
+  useEffect(() => {
+    if (isVisible) {
+      setFlipped(false);
+    }
+  }, [isVisible]);
+
+  const handleFlip = () => {
+    setFlipped(!flipped);
+    onClick();
+  };
+
   return (
-    <div
-      className={`certificate-card ${isSelected ? 'selected' : ''} ${
-        isVisible ? 'visible' : ''
-      }`}
-      onClick={onClick}
-    >
+    <div className={`certificate-card ${flipped ? 'flipped' : ''}`} onClick={handleFlip}>
       <div className="card-inner">
-        <div className={`front ${isSelected ? 'flipped' : ''}`}>
+        <div className={`front ${flipped ? 'flipped' : ''}`}>
           <img src={certificate.imageUrl} alt={certificate.text} />
+          <div className="certificate-info">
+            <p className="certificate-name">{certificate.text}</p>
+            <div className="credentials">
+              <p className="credentials-link">
+                <Button className="credentials-link" href={certificate.url} target="_blank" rel="noopener noreferrer">
+                  Credentials
+                </Button>
+              </p>
+            </div>
+          </div>
         </div>
-        <div className={`back ${isSelected ? 'flipped' : ''}`}>
-          <p>{certificate.text}</p>
-          <a href={certificate.url} target="_blank" rel="noopener noreferrer">
-            <button>Credentials</button>
-          </a>
+        <div className={`back ${flipped ? 'flipped' : ''}`}>
+          <p>{certificate.additionalInfo}</p>
         </div>
       </div>
     </div>
